e1aece11ad40175bd4592054a52ebc17984a6b2f
Jane Al-Shihabi <janealshihabi@gmail.com>
2024-01-10 13:24:04 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/motdujour-welcome.webp';
+	import welcome_fallback from '$lib/images/motdujour-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Mot du Jour</title>
+	<meta name="description" content="MDJ Svelte app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+	</h1>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
